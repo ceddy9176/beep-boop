085fc47006246eecb1ca6f1207a6d98fc6568932
connor miller <ceddy9176@gmail.com>
2017-10-27 11:23:02 -0700

    it will add back end logic to scripts.js

--- README.md
+++ README.md
@@ -15,11 +15,14 @@ Program will take a user input in the form of a number (0 and up), randomly gene
 
 ## Specs
 
-> User input results in a number containing: 1 --> output: "Boop!"
----
-> User input results in a number containing: 0 --> output: "Beep!"
----
-> User inputs a number divisible by 3  --> output: a string "I'm sorry, Dave. I'm afraid I can't do that."
+> User input results in a number containing: 1
+    -Output: "Boop!"
+
+> User input results in a number containing: 0
+    -Output: "Beep!"
+
+> User inputs a number divisible by 3  
+    -Output: a string "I'm sorry, Dave. I'm afraid I can't do that."
 
 
 ## known bugs
@@ -40,5 +43,5 @@ Program will take a user input in the form of a number (0 and up), randomly gene
 
 ## license
  Copyright (C) Connor Miller
- 
+
 Open source. Feel free to pull, copy, edit, etc.
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
+
+// back end logic
+var beepBoop = function (input, name) {
+  var newArray = [];
+  for (index = 0; index <= input; index += 1) {
+    if (index % 100 === 10) {
+      newArray.push("Boop-Beep!");
+    } else if (index % 3 === 0 && index !== 0) {
+      newArray.push("I'm sorry, " + name + ". I'm afraid I can't do that.");
+    } else if (index === 0 || index % 10 === 0) {
+      newArray.push("Beep!");
+    } else if (index === 1 || index % 10 === 1 || (index > 10 && index < 20)) {
+      newArray.push("Boop!");
+    } else {
+      newArray.push(index);
+    }
+  }
+  return newArray;
+}
+
 // Front end Logic
 $(document).ready(function() {
     $("#userInput").submit(function(event) {
      event.preventDefault();
-     var userInput = $("#userIput").val();
-     var result = beepBoop(userInput);
+     var userInput = $("#userInput").val();
+     var userName = $("#userName").val();
+     var result = beepBoop(input, name);
      $(".result").show();
      $("#output").text(result);
    });
